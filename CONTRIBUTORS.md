c3e45b864d0a76c5f73a6dc81dc51df933ef908a
Travis Weston <anubisthejackle@users.noreply.github.com>
2023-08-09 13:16:34 -0400

    Initial plugin files

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,9 @@
+# Contributors
+
+A heartfelt thank you to everyone who made this library possible:
+
+- Kevin Fodness ([@kevinfodness](https://github.com/kevinfodness))
+- Travis Weston ([@anubisthejackle](https://github.com/anubisthejackle))
+
+If you are interested in contributing, please read our
+[contributing guidelines](CONTRIBUTING.md).
---
+++ wp-block-migrator.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WP Block Migrator
+ * Plugin URI: https://github.com/alleyinteractive/wp-block-migrator
+ * Description: Utilities for assisting with Gutenberg migrations.
+ * Version: 0.1.0
+ * Author: alleyinteractive
+ * Author URI: https://alley.com/
+ * Requires at least: 6.2
+ * Tested up to: 6.2
+ *
+ * Text Domain: wp-block-migrator
+ * Domain Path: /languages/
+ *
+ * @package wp-block-migrator
+ */
